ef91fb91ce078329e3d2fbe3fdb04806322081cd
John Kouris <jdkouris@gmail.com>
2020-02-25 20:10:40 -0800

    Closes #1 Set up the project so UI is done entirely programmatically

---
+++ MyFitness/MyFitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFitness
+//
+//  Created by John Kouris on 2/25/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
