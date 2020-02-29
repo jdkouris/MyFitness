7fc4c3eb9db6952c5d7f23c3b2ad0666593ff688
John Kouris <jdkouris@gmail.com>
2020-02-29 10:15:33 -0800

    Set up the constraints to display necessary UI elements Configured the picker view to display the different workout types

--- MyFitness/MyFitness/Screens/AddWorkoutVC.swift
+++ MyFitness/MyFitness/Screens/AddWorkoutVC.swift
@@ -11,24 +11,116 @@ import UIKit
 class AddWorkoutVC: UIViewController {
     
     let tableView = UITableView()
+    let workoutTypeLabel = MFTitleLabel(textAlignment: .left, fontSize: 24)
     let workoutTypePickerView = UIPickerView()
+    let notesLabel = MFTitleLabel(textAlignment: .left, fontSize: 24)
+    let workoutNotesTextView = UITextView()
+    let addExercisesLabel = MFTitleLabel(textAlignment: .left, fontSize: 24)
+    
+    var exercises = [Exercise]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureVC()
+        configureWorkoutTypeLabel()
+        configureWorkoutType()
+        configureNotesLabel()
+        configureWorkoutNotes()
+        configureExercisesLabel()
         configureTableView()
+        
+        mockExerciseLoad()
+//        createDismissKeyboardTapGesture()
+    }
+    
+    func mockExerciseLoad() {
+        exercises.append(Exercise(name: "Bench Press", reps: 30, weight: 20))
     }
     
     func configureVC() {
         view.backgroundColor = .systemBackground
-        title = "Add Exercises"
-        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Add Workout Details"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        let saveBarButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtonTapped))
+        navigationItem.rightBarButtonItem = saveBarButton
     }
     
-    func configureTableView() {
+    @objc func saveButtonTapped() {
+        
+    }
+    
+    private func configureWorkoutTypeLabel() {
+        view.addSubview(workoutTypeLabel)
+        workoutTypeLabel.text = "WORKOUT TYPE"
+        
+        NSLayoutConstraint.activate([
+            workoutTypeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            workoutTypeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            workoutTypeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            workoutTypeLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    private func configureWorkoutType() {
+        view.addSubview(workoutTypePickerView)
+        workoutTypePickerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.workoutTypePickerView.delegate = self
+        self.workoutTypePickerView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            workoutTypePickerView.topAnchor.constraint(equalTo: workoutTypeLabel.bottomAnchor, constant: 8),
+            workoutTypePickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            workoutTypePickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            workoutTypePickerView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    private func configureNotesLabel() {
+        view.addSubview(notesLabel)
+        notesLabel.text = "NOTES"
+        
+        NSLayoutConstraint.activate([
+            notesLabel.topAnchor.constraint(equalTo: workoutTypePickerView.bottomAnchor, constant: 8),
+            notesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            notesLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            notesLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    private func configureWorkoutNotes() {
+        view.addSubview(workoutNotesTextView)
+        workoutNotesTextView.translatesAutoresizingMaskIntoConstraints = false
+        workoutNotesTextView.layer.cornerRadius = 10
+        workoutNotesTextView.layer.borderWidth = 1
+        workoutNotesTextView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2).cgColor
+        
+        NSLayoutConstraint.activate([
+            workoutNotesTextView.topAnchor.constraint(equalTo: notesLabel.bottomAnchor, constant: 8),
+            workoutNotesTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            workoutNotesTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            workoutNotesTextView.heightAnchor.constraint(equalToConstant: view.bounds.height / 9)
+        ])
+    }
+    
+    private func configureExercisesLabel() {
+        view.addSubview(addExercisesLabel)
+        addExercisesLabel.text = "Add Exercises"
+        
+        NSLayoutConstraint.activate([
+            addExercisesLabel.topAnchor.constraint(equalTo: workoutNotesTextView.bottomAnchor, constant: 16),
+            addExercisesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            addExercisesLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            addExercisesLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    private func configureTableView() {
         view.addSubview(tableView)
-        tableView.frame = view.bounds
-        tableView.rowHeight = 80
+        tableView.frame = CGRect(x: 0, y: view.bounds.height / 2, width: view.bounds.width, height: view.bounds.height)
+        tableView.rowHeight = 120
+//        tableView.separatorStyle = .none
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -36,27 +128,42 @@ class AddWorkoutVC: UIViewController {
         tableView.register(WorkoutCell.self, forCellReuseIdentifier: WorkoutCell.reuseID)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createDismissKeyboardTapGesture() {
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
     }
-    */
 
 }
 
 extension AddWorkoutVC: UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return exercises.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: WorkoutCell.reuseID) as? WorkoutCell else { return UITableViewCell() }
+        
+        let exercise = exercises[indexPath.row]
+        cell.set(exercise: exercise)
+        
         return cell
     }
     
+}
+
+extension AddWorkoutVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return WorkoutType.allCases.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return WorkoutType.allCases[row].description
+    }
     
 }
