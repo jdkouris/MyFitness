75cb7fbabe9cc3feb0c4cd5c3fe3458245e36f0a
John Kouris <jdkouris@gmail.com>
2020-02-29 10:13:55 -0800

    Created a custom MFTextField class

---
+++ MyFitness/MyFitness/Custom Views/TextFields/MFTextField.swift
@@ -0,0 +1,42 @@
+//
+//  MFTextField.swift
+//  MyFitness
+//
+//  Created by John Kouris on 2/29/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        returnKeyType = .go
+        clearButtonMode = .whileEditing
+    }
+
+}
