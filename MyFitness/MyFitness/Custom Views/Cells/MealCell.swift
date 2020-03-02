e2c4b23edb62a9edf4b23ce9b5b794cb78a9da06
John Kouris <jdkouris@gmail.com>
2020-03-02 09:16:10 -0800

    Added the MealCell and the MFMealImageView files

---
+++ MyFitness/MyFitness/Custom Views/Cells/MealCell.swift
@@ -0,0 +1,14 @@
+//
+//  MealCell.swift
+//  MyFitness
+//
+//  Created by John Kouris on 3/2/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MealCell: UICollectionViewCell {
+    static let reuseID = "MealCell"
+    let mealImageView = UIImageView(frame: .zero)
+}
---
+++ MyFitness/MyFitness/Custom Views/ImageViews/MFMealImageView.swift
@@ -0,0 +1,15 @@
+//
+//  MFMealImageView.swift
+//  MyFitness
+//
+//  Created by John Kouris on 3/2/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MFMealImageView: UIImageView {
+
+    
+
+}
