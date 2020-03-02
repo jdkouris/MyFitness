0d2a8697b377d250e83c832f3ff3d6231a261a2d
John Kouris <jdkouris@gmail.com>
2020-03-02 09:25:42 -0800

    Added the food-placeholder image and filled out the MFMealImageView

--- MyFitness/MyFitness/Custom Views/ImageViews/MFMealImageView.swift
+++ MyFitness/MyFitness/Custom Views/ImageViews/MFMealImageView.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class MFMealImageView: UIImageView {
 
-    
+    let placeholderImage = UIImage(named: "food-placeholder")
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
