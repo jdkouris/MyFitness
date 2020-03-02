0c88286a7359208afeb2e6bffd9678dfa67a694f
John Kouris <jdkouris@gmail.com>
2020-03-02 09:09:24 -0800

    Added the MealDetailVC and the UIHelper

---
+++ MyFitness/MyFitness/Screens/MealDetailsVC.swift
@@ -0,0 +1,19 @@
+//
+//  MealDetailsVC.swift
+//  MyFitness
+//
+//  Created by John Kouris on 3/2/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MealDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ MyFitness/MyFitness/Screens/UIHelper.swift
@@ -0,0 +1,9 @@
+//
+//  UIHelper.swift
+//  MyFitness
+//
+//  Created by John Kouris on 3/2/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
