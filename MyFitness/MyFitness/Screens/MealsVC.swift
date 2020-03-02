1405c245bccbb0b662f9c94760a97e73d9b6b1c6
John Kouris <jdkouris@gmail.com>
2020-03-02 10:02:12 -0800

    Added various methods to the MealsVC

--- MyFitness/MyFitness/Screens/MealsVC.swift
+++ MyFitness/MyFitness/Screens/MealsVC.swift
@@ -9,22 +9,113 @@
 import UIKit
 
 class MealsVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
+    
+    enum Section {
+        case main
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var recipeName: String!
+    var recipes: [Recipe] = []
+    var filteredRecipes: [Recipe] = []
+    var page = 1
+    var hasMoreRecipes = true
+    var isSearching = false
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Recipe>!
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        configureSearchController()
+        
+        configureDataSource()
+    }
+    
+    private func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createTwoColumnFlowLayout(in: view))
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(MealCell.self, forCellWithReuseIdentifier: MealCell.reuseID)
+    }
+    
+    func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search for a recipe"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Recipe>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, recipe) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MealCell.reuseID, for: indexPath) as! MealCell
+            cell.set(recipe: recipe)
+            return cell
+        })
+    }
+    
+    func updateData(on recipes: [Recipe]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Recipe>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(recipes)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true, completion: nil)
+        }
+    }
 
+}
+
+extension MealsVC: UICollectionViewDelegate {
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - height {
+            guard hasMoreRecipes else { return }
+            page += 1
+            // getRecipes()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray = isSearching ? filteredRecipes : recipes
+        let recipe = activeArray[indexPath.item]
+        
+        let destinationVC = MealDetailsVC()
+        
+        let navController = UINavigationController(rootViewController: destinationVC)
+        present(navController, animated: true, completion: nil)
+    }
+}
+
+extension MealsVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        isSearching = true
+        filteredRecipes = recipes.filter { $0.title.lowercased().contains(filter.lowercased()) }
+        updateData(on: filteredRecipes)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
+        updateData(on: recipes)
+    }
 }
