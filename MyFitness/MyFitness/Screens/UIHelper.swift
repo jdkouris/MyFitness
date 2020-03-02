e7c1d622df88cb46170bd84a9107c34df8579e69
John Kouris <jdkouris@gmail.com>
2020-03-02 09:13:00 -0800

    Added the createTwoColumnFlowLayout method

--- MyFitness/MyFitness/Screens/UIHelper.swift
+++ MyFitness/MyFitness/Screens/UIHelper.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 John Kouris. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct UIHelper {
+    
+    static func createTwoColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 2
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 20)
+        
+        return flowLayout
+    }
+    
+}
