c70932d36a70b4b9f52525b9e04b37fbfc81fc92
John Kouris <jdkouris@gmail.com>
2020-03-04 13:35:41 -0800

    Added the MFLineChart file

---
+++ MyFitness/MyFitness/Custom Views/Chart Views/MFLineChart.swift
@@ -0,0 +1,15 @@
+//
+//  MFLineChart.swift
+//  MyFitness
+//
+//  Created by John Kouris on 3/4/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MFLineChart: UIView {
+
+    
+
+}
