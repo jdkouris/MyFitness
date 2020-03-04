5006915e7bdbcd7c72be039a4138c838517722ba
John Kouris <jdkouris@gmail.com>
2020-03-04 14:50:21 -0800

    Removed the MFLineChart file Changed to a bar chart for ProgressVC

--- MyFitness/MyFitness/Screens/ProgressVC.swift
+++ MyFitness/MyFitness/Screens/ProgressVC.swift
@@ -11,12 +11,14 @@ import Charts
 
 class ProgressVC: UIViewController {
     
-    let lineChartView = LineChartView()
+    let barChartView = BarChartView()
+    let months = ["January", "February", "March", "April", "May", "June"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         configureVC()
-        customizeChart(dataPoints: ["Jan", "Feb", "Mar", "Apr", "May"], values: [0,1,2,3,4])
+        customizeChart(dataPoints: months, values: [195.0,202.0,200.0,205.0,199.0,200.0].map { Double($0) })
         layoutUIElements()
     }
     
@@ -33,27 +35,30 @@ class ProgressVC: UIViewController {
     }
     
     func customizeChart(dataPoints: [String], values: [Double]) {
-        var dataEntries: [ChartDataEntry] = []
-        
+        var dataEntries: [BarChartDataEntry] = []
         for i in 0..<dataPoints.count {
-            let dataEntry = ChartDataEntry(x: values[i], y: Double(i))
+            let dataEntry = BarChartDataEntry(x: Double(i), y: Double(values[i]))
             dataEntries.append(dataEntry)
         }
         
-        let lineChartDataSet = LineChartDataSet(entries: dataEntries, label: nil)
-        let lineChartData = LineChartData(dataSet: lineChartDataSet)
-        lineChartView.data = lineChartData
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Bar Chart View")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        barChartView.data = chartData
+        
+//        barChartView.animate(yAxisDuration: 1.0)
+        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: months)
+        barChartView.fitBars = true
     }
     
     func layoutUIElements() {
-        lineChartView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(lineChartView)
+        barChartView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(barChartView)
         
         NSLayoutConstraint.activate([
-            lineChartView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            lineChartView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            lineChartView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            lineChartView.heightAnchor.constraint(equalToConstant: 400)
+            barChartView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            barChartView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            barChartView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            barChartView.heightAnchor.constraint(equalToConstant: 400)
         ])
     }
     
