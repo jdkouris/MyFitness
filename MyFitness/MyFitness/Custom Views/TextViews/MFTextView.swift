2488e258fafb2d83d70e31ddb0164fc1e2e14c5d
John Kouris <jdkouris@gmail.com>
2020-03-03 18:15:01 -0800

    Added the MFTextView

---
+++ MyFitness/MyFitness/Custom Views/TextViews/MFTextView.swift
@@ -0,0 +1,21 @@
+//
+//  MFTextView.swift
+//  MyFitness
+//
+//  Created by John Kouris on 3/3/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MFTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
