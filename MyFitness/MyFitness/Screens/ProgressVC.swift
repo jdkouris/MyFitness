14a0936d28d61aa54ec1daab44c907420a09dc98
John Kouris <jdkouris@gmail.com>
2020-03-03 20:01:54 -0800

    Added dummy chart to test functionality

--- MyFitness/MyFitness/Screens/ProgressVC.swift
+++ MyFitness/MyFitness/Screens/ProgressVC.swift
@@ -7,24 +7,54 @@
 //
 
 import UIKit
+import Charts
 
 class ProgressVC: UIViewController {
+    
+    let lineChartView = LineChartView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureVC()
+        customizeChart(dataPoints: ["Jan", "Feb", "Mar", "Apr", "May"], values: [0,1,2,3,4])
+        layoutUIElements()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureVC() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addBarButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addBarButton
     }
-    */
-
+    
+    @objc func addButtonTapped() {
+        
+    }
+    
+    func customizeChart(dataPoints: [String], values: [Double]) {
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: values[i], y: Double(i))
+            dataEntries.append(dataEntry)
+        }
+        
+        let lineChartDataSet = LineChartDataSet(entries: dataEntries, label: nil)
+        let lineChartData = LineChartData(dataSet: lineChartDataSet)
+        lineChartView.data = lineChartData
+    }
+    
+    func layoutUIElements() {
+        lineChartView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(lineChartView)
+        
+        NSLayoutConstraint.activate([
+            lineChartView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            lineChartView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            lineChartView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            lineChartView.heightAnchor.constraint(equalToConstant: 400)
+        ])
+    }
+    
 }
