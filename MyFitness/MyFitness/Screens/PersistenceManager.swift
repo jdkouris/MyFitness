1af8c296d4414cbd65da3af4584d1eb9dcd31993
John Kouris <jdkouris@gmail.com>
2020-03-05 13:30:46 -0800

    Added the PersistenceManager file

---
+++ MyFitness/MyFitness/Screens/PersistenceManager.swift
@@ -0,0 +1,17 @@
+//
+//  PersistenceManager.swift
+//  MyFitness
+//
+//  Created by John Kouris on 3/5/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add
+}
+
+enum PersistenceManager {
+    
+}
