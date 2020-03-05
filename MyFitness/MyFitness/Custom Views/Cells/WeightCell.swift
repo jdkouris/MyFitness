64bad79435e864acc982bec68ac5d418a7b1ec60
John Kouris <jdkouris@gmail.com>
2020-03-04 19:06:57 -0800

    Added the WeightCell

---
+++ MyFitness/MyFitness/Custom Views/Cells/WeightCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeightCell.swift
+//  MyFitness
+//
+//  Created by John Kouris on 3/4/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class WeightCell: UITableViewCell {
+
+    
+
+}
