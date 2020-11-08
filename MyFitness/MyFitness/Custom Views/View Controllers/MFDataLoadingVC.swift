330104f70aa54e55e9dd6fab73a2ffa636e2ee2b
John Kouris <jdkouris@gmail.com>
2020-11-08 14:15:11 -0800

    Added MFDataLoadingVC file

---
+++ MyFitness/MyFitness/Custom Views/View Controllers/MFDataLoadingVC.swift
@@ -0,0 +1,30 @@
+//
+//  MFDataLoadingVC.swift
+//  MyFitness
+//
+//  Created by John Kouris on 11/8/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MFDataLoadingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
