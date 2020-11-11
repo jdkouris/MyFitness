bca4ae264c29c609f156d880d8b0e3550e207224
John Kouris <jdkouris@gmail.com>
2020-11-10 17:50:24 -0800

    Added cardView styling to the MealCells

--- MyFitness/MyFitness/Custom Views/Cells/MealCell.swift
+++ MyFitness/MyFitness/Custom Views/Cells/MealCell.swift
@@ -43,18 +43,32 @@ class MealCell: UICollectionViewCell {
     }
     
     private func configure() {
-        addSubview(mealImageView)
-        addSubview(mealTitleLabel)
+        let cardView = UIView(frame: self.contentView.frame)
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+        cardView.backgroundColor = .systemBackground
+        cardView.layer.cornerRadius = 10
+        cardView.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        cardView.layer.shadowOpacity = 1
+        cardView.layer.shadowOffset = .zero
+        cardView.layer.shadowRadius = 4
+        
+        addSubview(cardView)
+        cardView.addSubviews(mealImageView, mealTitleLabel)
         
         let padding: CGFloat = 8
         
         NSLayoutConstraint.activate([
+            cardView.topAnchor.constraint(equalTo: self.topAnchor, constant: padding / 2),
+            cardView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding / 2),
+            cardView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding / 2),
+            cardView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -padding / 2),
+            
             mealImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
             mealImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
             mealImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
             mealImageView.heightAnchor.constraint(equalTo: mealImageView.widthAnchor),
             
-            mealTitleLabel.topAnchor.constraint(equalTo: mealImageView.bottomAnchor, constant: padding),
+            mealTitleLabel.topAnchor.constraint(equalTo: mealImageView.bottomAnchor),
             mealTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
             mealTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
             mealTitleLabel.heightAnchor.constraint(equalToConstant: 20)
