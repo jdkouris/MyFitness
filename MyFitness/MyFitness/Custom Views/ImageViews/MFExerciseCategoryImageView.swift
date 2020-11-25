546ec31125dd93589ed1aa4f766a45bbcb7ddf28
John Kouris <jdkouris@gmail.com>
2020-11-24 19:20:53 -0800

    Updated the MFExerciseCategoryImageView - will need to update placeholder image in the future

--- MyFitness/MyFitness/Custom Views/ImageViews/MFExerciseCategoryImageView.swift
+++ MyFitness/MyFitness/Custom Views/ImageViews/MFExerciseCategoryImageView.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class MFExerciseCategoryImageView: UIImageView {
 
+    let placeholderImage = UIImage(named: "food-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 
 }
