659ebfa0e210575e05c6be15170c58a974cfc6b4
John Kouris <jdkouris@gmail.com>
2020-11-24 18:06:43 -0800

    Added MFExerciseCategoryImageView file to the project

---
+++ MyFitness/MyFitness/Custom Views/ImageViews/MFExerciseCategoryImageView.swift
@@ -0,0 +1,15 @@
+//
+//  MFExerciseCategoryImageView.swift
+//  MyFitness
+//
+//  Created by John Kouris on 11/24/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MFExerciseCategoryImageView: UIImageView {
+
+    
+
+}
