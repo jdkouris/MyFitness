291d89f0b496b7a97cdfa2ff4758d4248ae5a90e
John Kouris <jdkouris@gmail.com>
2020-11-25 20:54:11 -0800

    Added the ExerciseCell file to the project

---
+++ MyFitness/MyFitness/Custom Views/Cells/ExerciseCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExerciseCell.swift
+//  MyFitness
+//
+//  Created by John Kouris on 11/25/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
