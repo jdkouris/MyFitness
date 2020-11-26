7ca3e079f42332ba862262dea5269c885a80ee64
John Kouris <jdkouris@gmail.com>
2020-11-25 21:06:54 -0800

    Configured the ExerciseCell

--- MyFitness/MyFitness/Custom Views/Cells/ExerciseCell.swift
+++ MyFitness/MyFitness/Custom Views/Cells/ExerciseCell.swift
@@ -9,16 +9,58 @@
 import UIKit
 
 class ExerciseCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let reuseID = "ExerciseCell"
+    let exerciseNameLabel = MFTitleLabel(textAlignment: .left, fontSize: 20)
+    let exerciseDescriptionLabel = MFBodyLabel(textAlignment: .left)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+    
+    func set(exercise: DemoExercise) {
+        exerciseNameLabel.text = exercise.name
+        exerciseDescriptionLabel.text = exercise.description
+    }
+    
+    private func configure() {
+        let cardView = UIView(frame: self.contentView.frame)
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+        cardView.backgroundColor = .systemBackground
+        cardView.layer.cornerRadius = 10
+        cardView.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        cardView.layer.shadowOpacity = 1
+        cardView.layer.shadowOffset = .zero
+        cardView.layer.shadowRadius = 4
+        
+        exerciseDescriptionLabel.textAlignment = .natural
+        selectionStyle = .none
+        
+        addSubview(cardView)
+        cardView.addSubviews(exerciseNameLabel, exerciseDescriptionLabel)
+        
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            cardView.topAnchor.constraint(equalTo: self.topAnchor, constant: padding / 2),
+            cardView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            cardView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            cardView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -padding / 2),
+            
+            exerciseNameLabel.topAnchor.constraint(equalTo: cardView.topAnchor, constant: padding),
+            exerciseNameLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: padding),
+            exerciseNameLabel.trailingAnchor.constraint(equalTo: cardView.trailingAnchor),
+            exerciseNameLabel.heightAnchor.constraint(equalToConstant: 30),
 
+            exerciseDescriptionLabel.topAnchor.constraint(equalTo: exerciseNameLabel.bottomAnchor),
+            exerciseDescriptionLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: padding),
+            exerciseDescriptionLabel.trailingAnchor.constraint(equalTo: cardView.trailingAnchor),
+            exerciseDescriptionLabel.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -padding)
+        ])
+    }
+    
 }
