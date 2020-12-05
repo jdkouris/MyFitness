b81fe32c6dfceb6a4ef687eb33743ca36cfa37ff
John Kouris <jdkouris@gmail.com>
2020-12-04 18:47:25 -0800

    Configured UIViewController extension

--- MyFitness/MyFitness/Extensions/UIViewController+Ext.swift
+++ MyFitness/MyFitness/Extensions/UIViewController+Ext.swift
@@ -7,3 +7,16 @@
 //
 
 import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = MFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+    
+}
