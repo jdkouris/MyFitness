579e76585062bd3749c8ac1a8094b023ee48a14a
John Kouris <jdkouris@gmail.com>
2020-12-04 18:42:36 -0800

    Configured the MFButton

--- MyFitness/MyFitness/Custom Views/Buttons/MFButton.swift
+++ MyFitness/MyFitness/Custom Views/Buttons/MFButton.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class MFButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(backgroundColor: UIColor, title: String) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func set(backgroundColor: UIColor, title: String) {
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
 
 }
