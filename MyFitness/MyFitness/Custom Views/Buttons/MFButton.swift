4987b737aaf60c607a4fcb523c0586e7f0daee7c
John Kouris <jdkouris@gmail.com>
2020-12-04 18:41:58 -0800

    Added MFButton file

---
+++ MyFitness/MyFitness/Custom Views/Buttons/MFButton.swift
@@ -0,0 +1,21 @@
+//
+//  MFButton.swift
+//  MyFitness
+//
+//  Created by John Kouris on 12/4/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
