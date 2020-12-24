5d65a79ab539c77d150c0d30f7ddeae49fed2a98
John Kouris <jdkouris@gmail.com>
2020-12-24 14:58:58 -0800

    Configured the LocationManager

--- MyFitness/MyFitness/Managers/LocationManager.swift
+++ MyFitness/MyFitness/Managers/LocationManager.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import MapKit
+
+class LocationManager: NSObject {
+    static let shared = LocationManager()
+    var locationManager = CLLocationManager()
+    var onLocationUpdate: ((CLLocation) -> Void)?
+    
+    func start(completionHandler: @escaping (CLLocation) -> Void) {
+        locationManager.requestWhenInUseAuthorization()
+
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.distanceFilter = 100.0
+        locationManager.delegate = self
+
+        locationManager.startUpdatingLocation()
+
+        onLocationUpdate = completionHandler
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let newLocation = locations.last else { return }
+
+        onLocationUpdate?(newLocation)
+    }
+}
