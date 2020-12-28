2108ffd87647c6e7951916b433ec8c124efeef99
John Kouris <jdkouris@gmail.com>
2020-12-28 09:21:32 -0800

    Added the Place model

---
+++ MyFitness/MyFitness/Models/Place.swift
@@ -0,0 +1,16 @@
+//
+//  Place.swift
+//  MyFitness
+//
+//  Created by John Kouris on 12/28/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    let name: String
+    let address: String
+    let latitude: Double
+    let longitude: Double
+}
