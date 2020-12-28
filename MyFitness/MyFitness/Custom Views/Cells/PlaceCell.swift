1b35e15bda3bfb307ec07b66c28f8ed2620d4a93
John Kouris <jdkouris@gmail.com>
2020-12-28 09:11:10 -0800

    Added PlaceCell to the project

---
+++ MyFitness/MyFitness/Custom Views/Cells/PlaceCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlaceCell.swift
+//  MyFitness
+//
+//  Created by John Kouris on 12/28/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class PlaceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
